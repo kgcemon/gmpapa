ed18f43e27feb21f7ad7891a9e119dd3d71e3505
kgcemon <kgcemonbd@gmail.com>
2025-09-04 22:56:39 -0700

    slider images update fixed

--- app/Models/HelpLine.php
+++ app/Models/HelpLine.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class HelpLine extends Model
 {
     protected $table = 'help_line';
+
+    protected $fillable = [
+      'name',
+      'image',
+      'url'
+    ];
 }
