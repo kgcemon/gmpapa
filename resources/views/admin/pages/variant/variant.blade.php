bd1007ace8df1cd6f5213bb5e3ab9456d3f37362
kgcemon <kgcemonbd@gmail.com>
2025-09-12 22:20:31 -0700

    Initial commit

--- resources/views/admin/pages/variant/variant.blade.php
+++ resources/views/admin/pages/variant/variant.blade.php
@@ -176,7 +176,7 @@
     </div>
 @endsection
 
-@push('scripts')}}
+@push('scripts')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
     <script>
