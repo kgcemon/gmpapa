9820afd68a876fb708aff007ff9076513ce15934
kgcemon <kgcemonbd@gmail.com>
2025-09-19 04:00:44 -0700

    updated

--- app/Http/Controllers/admin/UsersController.php
+++ app/Http/Controllers/admin/UsersController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Mail\MoneyReceivedMail;
 use App\Models\User;
+use App\Models\WalletTransaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\Rule;
 
 class UsersController extends Controller
@@ -86,7 +89,50 @@ class UsersController extends Controller
 
         $user->name = $validatedData['name'];
         $user->email = $validatedData['email'];
-        $user->wallet += $validatedData['wallet'] ?? $user->wallet;
+        $oldBalance = $user->wallet;
+        if (isset($validatedData['wallet'])) {
+
+            $amount = $validatedData['wallet'];
+
+            $user->wallet = $amount;
+
+            if($oldBalance > $amount) {
+                WalletTransaction::create([
+                    'user_id'   => $user->id,
+                    'amount'    => $oldBalance - $amount,
+                    'type'      => 'debit',
+                    'description' => 'Admin debit balance',
+                    'status'    => 1,
+                ]);
+                try {
+                    Mail::to($user->email)->send(new MoneyReceivedMail(
+                        $user->name,
+                        'TXN-' . strtoupper(rand(10000,10000)),
+                        now()->format('d M Y, h:i A'),
+                        $amount,
+                        url('/')
+                    ));
+                }catch (\Exception $exception){}
+
+            }else{
+                WalletTransaction::create([
+                    'user_id'   => $user->id,
+                    'amount'    => $amount-$oldBalance,
+                    'type'      => 'credit',
+                    'description' => 'Admin added menualy your',
+                    'status'    => 1,
+                ]);
+                try {
+                    Mail::to($user->email)->send(new MoneyReceivedMail(
+                        $user->name,
+                        'TXN-' . strtoupper(rand(10000,10000)),
+                        now()->format('d M Y, h:i A'),
+                        $amount,
+                        url('/')
+                    ));
+                }catch (\Exception $exception){}
+            }
+        }
 
         if (!empty($validatedData['password'])) {
             $user->password = Hash::make($validatedData['password']);
