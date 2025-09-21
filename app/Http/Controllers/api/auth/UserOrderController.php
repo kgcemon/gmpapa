7e26dd49b97009d553a135c30344ffed6cbf6d14
kgcemon <kgcemonbd@gmail.com>
2025-09-20 21:33:18 -0700

    first commit

--- app/Http/Controllers/api/auth/UserOrderController.php
+++ app/Http/Controllers/api/auth/UserOrderController.php
@@ -11,7 +11,7 @@ class UserOrderController extends Controller
     public function userOrder(Request $request)
     {
         $user = $request->user();
-        $orders = Order::where('user_id',$user->id)->with('paymentMethod','item')->orderBy('created_at','desc')->paginate(15);
+        $orders = Order::where('user_id',$user->id)->with('paymentMethod','product','item')->orderBy('created_at','desc')->paginate(15);
         return response()->json([
             'status'  => true,
             'message' => 'order list',
