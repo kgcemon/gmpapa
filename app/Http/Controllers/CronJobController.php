3e6d5622011ca652d59431592021bd536a46dd75
kgcemon <kgcemonbd@gmail.com>
2025-09-21 07:48:53 -0700

    code system updated

--- app/Http/Controllers/CronJobController.php
+++ app/Http/Controllers/CronJobController.php
@@ -141,6 +141,7 @@ class CronJobController extends Controller
         $pins = $codes->map(function ($code) {
             return [
                 'pin'    => $code->code,
+                'amount' => $code->items()->name??  ".",
             ];
         })->toArray();
 
