f48a0f1c6c42f0effd97001b59cee42353a81e37
kgcemon <kgcemonbd@gmail.com>
2025-09-26 05:43:35 -0700

    updated

--- app/Http/Controllers/api/ProductsController.php
+++ app/Http/Controllers/api/ProductsController.php
@@ -9,14 +9,24 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function index(){
-
+    public function index()
+    {
         $products = Categorie::with(['products' => function ($q) {
-            $q->withCount('reviews'); // ekhane review_count ashbe
+            $q->withCount('reviews')
+            ->withAvg('reviews', 'rating');
         }])
             ->select('name','id')
             ->orderBy('sort')
             ->paginate(50);
+
+        $products->getCollection()->transform(function ($category) {
+            $category->products->transform(function ($product) {
+                $product->reviews_avg_rating = round($product->reviews_avg_rating, 1);
+                return $product;
+            });
+            return $category;
+        });
+
         return response()->json([
             'status' => true,
             'data' => $products->items(),
@@ -25,6 +35,7 @@ class ProductsController extends Controller
             'last_page' => $products->lastPage(),
         ]);
     }
+
 
     public function show($slug)
     {
