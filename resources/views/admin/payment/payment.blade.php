c303de65f44b5864bc8b27e2c1b1904af91f4729
kgcemon <kgcemonbd@gmail.com>
2025-10-01 08:12:16 -0700

    updated

--- resources/views/admin/payment/payment.blade.php
+++ resources/views/admin/payment/payment.blade.php
@@ -221,9 +221,7 @@
                 selector: '.tinymce-editor',
                 plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount',
                 toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-                menubar: true,
-                height: 200,
-                content_style: "body { font-family:Arial,sans-serif; font-size:14px }"
+                height: 300,
             });
         });
     </script>
