5860fdb2f23e07657747aeb6d593c27e7eb1f139
kgcemon <kgcemonbd@gmail.com>
2025-10-05 20:39:07 -0700

    updated

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -11,24 +11,10 @@ class SitemapController extends Controller
     {
         $urls = [];
 
-        $urls[] = [
-            'loc' => url('/'),
-            'lastmod' => Carbon::now()->toAtomString(),
-            'changefreq' => 'daily',
-            'priority' => '1.0'
-        ];
-
-        $urls[] = [
-            'loc' => url('/about'),
-            'lastmod' => Carbon::now()->toAtomString(),
-            'changefreq' => 'monthly',
-            'priority' => '0.8'
-        ];
-
         $products = Product::where('name', '!=', 'Wallet')->get();
         foreach ($products as $product) {
             $urls[] = [
-                'loc' => url('/product/' . $product->slug),
+                'loc' => 'https://freefirebd.com/product/' . $product->slug,
                 'lastmod' => optional($product->updated_at)->toAtomString() ?? now()->toAtomString(),
                 'changefreq' => 'weekly',
                 'priority' => '1.0'
