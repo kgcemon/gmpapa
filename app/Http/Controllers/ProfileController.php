c3a4b2eb16b95c670295ccfc1db4b7b5cd36c42d
kgcemon <kgcemonbd@gmail.com>
2025-11-28 19:54:50 -0800

    updated

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -30,17 +30,24 @@ class ProfileController extends Controller
     /**
      * Update the user's profile information.
      */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
+    public function update(Request $request)
     {
-        $request->user()->fill($request->validated());
+        $request->validate([
+            'current_password' => 'required',
+            'password' => 'required|min-8|confirmed',
+        ]);
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        $admin = auth()->user();
+
+        if (!Hash::check($request->current_password, $admin->password)) {
+            return back()->with('error', 'Current password does not match.');
         }
 
-        $request->user()->save();
+        $admin->update([
+            'password' => Hash::make($request->password),
+        ]);
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return back()->with('status', 'Password successfully updated.');
     }
 
     /**
